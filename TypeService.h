978a96db6c1da64d5be94004bbb04cc7643de437
sandress2998 <149189942+sandress2998@users.noreply.github.com>
2024-04-05 15:48:54 +0300

    Update TypeService.h
    setVector***Info functions removed.

--- TypeService.h
+++ TypeService.h
@@ -1,28 +1,28 @@
 #pragma once
 #include "FieldInfo.h"
+#include "Complex.h"
 
-FieldInfo* setIntOperationsService();
-FieldInfo* setFloatOperationsService();
-FieldInfo* setDoubleOperationsService();
-FieldInfo* setComplexOperationsService();
+void sumInt(const void*, const void*, void*);
+void multiplyInt(const void*, const void*, void*);
+void getIntValue(void*);
+void printInt(void*);
 
+void sumFloat(const void*, const void*, void*);
+void multiplyFloat(const void*, const void*, void*);
+void getFloatValue(void*);
+void printFloat(void*);
 
-void sumInt(const void* a, const void* b, void* res);
-void multiplyInt(const void* a, const void* b, void* res);
-void getIntValue(void* value);
-void printInt(void* value);
+void sumDouble(const void*, const void*, void*);
+void multiplyDouble(const void*, const void*, void*);
+void getDoubleValue(void*);
+void printDouble(void*);
 
-void sumFloat(const void* a, const void* b, void* res);
-void multiplyFloat(const void* a, const void* b, void* res;
-void getFloatValue(void* value);
-void printFloat(void* value);
+void sumComplex(const void*, const void*, void*);
+void multiplyComplex(const void*, const void*, void*);
+void getComplexValue(void*);
+void printComplex(void*);
 
-void sumDouble(const void* a, const void* b, void* res);
-void multiplyDouble(const void* a, const void* b, void* res);
-void getDoubleValue(void* value);
-void printDouble(void* value);
-
-void sumComplex(const void* a, const void* b, void* res);
-void multiplyComplex(const void* a, const void* b, void* res);
-void getComplexValue(void* value);
-void printComplex(void* value);
+static const FieldInfo intOperationsService = {&sumInt, &multiplyInt, &getIntValue, &printInt, sizeof(int)};
+static const FieldInfo floatOperationsService = {&sumFloat, &multiplyFloat, &getFloatValue, &printFloat, sizeof(float)};
+static const FieldInfo doubleOperationsService = {&sumDouble, &multiplyDouble, &getDoubleValue, &printDouble, sizeof(double)};
+static const FieldInfo complexOperationsService = {&sumComplex, &multiplyComplex, &getComplexValue, &printComplex, sizeof(Complex)};
